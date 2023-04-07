bf7b15cf2f5862850d091262c24c401fdab25871
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2023-04-07 22:08:49 +0200

    added a map to the homepage

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentHomeBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.MapController
+import org.osmdroid.views.overlay.OverlayItem
 
 /**
  * A simple [Fragment] subclass.
@@ -13,6 +20,11 @@ import com.example.myapplication.databinding.FragmentHomeBinding
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+
+    // OpenStreeMap
+    lateinit var mapController: MapController
+    lateinit var overlayItem: ArrayList<OverlayItem>
+    val location = GeoPoint(65.5840799,22.1975568)
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -26,7 +38,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
+        Configuration.getInstance().load(context, PreferenceManager.getDefaultSharedPreferences(context));
+        Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID;
+        binding.mapView.setMultiTouchControls(true)
+        binding.mapView.controller.animateTo(location)
+        binding.mapView.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
+        binding.mapView.setUseDataConnection(true)
+        binding.mapView.setTileSource(TileSourceFactory.WIKIMEDIA)
+        mapController = binding.mapView.controller as MapController
+        mapController.setCenter(location)
+        mapController.zoomTo(15)
         return binding.root
     }
     override fun onDestroyView() {
