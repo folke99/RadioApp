92319c0d4cd4c813fed989a9092c46efaffc1b3b
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2023-04-09 20:36:38 +0200

    adding a recyclerview to the favorites page

--- app/src/main/java/com/example/myapplication/FavoritesFragment.kt
+++ app/src/main/java/com/example/myapplication/FavoritesFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.FragmentFavoritesBinding
 
 /**
@@ -26,7 +30,14 @@ class FavoritesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
-
+        val recyclerView = binding.favoritesList
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        val data = ArrayList<ItemViewModel>()
+        for (i in 1..20) {
+            data.add(ItemViewModel(R.drawable.ic_no_image_light, "Channel" + i))
+        }
+        val adapter = FavoritesAdapter(data)
+        recyclerView.adapter = adapter
         return binding.root
     }
 
@@ -34,4 +45,44 @@ class FavoritesFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+class FavoritesAdapter(private val mList: List<ItemViewModel>) : RecyclerView.Adapter<FavoritesAdapter.ViewHolder>() {
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_channel_item, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val itemViewModel = mList[position]
+
+        // sets the image to the imageview from our itemHolder class
+        holder.imageView.setImageResource(itemViewModel.image)
+
+        // sets the text to the textview from our itemHolder class
+        holder.textView.text = itemViewModel.text
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.channelImage)
+        val textView: TextView = itemView.findViewById(R.id.channelText)
+    }
+}
+
+data class ItemViewModel(val image: Int, val text: String) {
 }
